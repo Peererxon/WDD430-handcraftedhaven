48d1374d8a3b84369c85741a747d1b92fd16e60b
andersongil <andersongil.dev@gmail.com>
2024-11-23 17:42:48 -0400

    IMP: Card for art
    - Using the Nextjs component for Cards and images
    - Responsive
    - Created container and item components individuals

---
+++ src/app/ui/Card.tsx
@@ -0,0 +1,37 @@
+import { Card, CardBody } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+
+function CardItem({
+	image,
+	title,
+	description,
+	price,
+}: {
+	image: string;
+	title: string;
+	description: string;
+	price: string;
+}) {
+	return (
+		<Card shadow="sm" isPressable className="bg-secondary-50 p-3 border">
+			<CardBody className="flex flex-col gap-3 sm:gap-6 sm:flex-row ">
+				<Image
+					src={image}
+					alt={title}
+					className="max-w-full"
+					width={300}
+				></Image>
+				<div className="sm:flex sm:flex-col sm:justify-center">
+					<p className="text-left text-xl">
+						<strong>{title}</strong>
+					</p>
+					<p className="hidden sm:block">{description}</p>
+
+					<p className=" text-md text-black text-center mt-2">{price}</p>
+				</div>
+			</CardBody>
+		</Card>
+	);
+}
+
+export default CardItem;
--- src/app/ui/Cards.tsx
+++ src/app/ui/Cards.tsx
@@ -1,7 +1,5 @@
-"use client";
-import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
 import { Divider } from "@nextui-org/divider";
-import { Image } from "@nextui-org/image";
+import CardItem from "./Card";
 export default function Cards() {
 	const list = [
 		{
@@ -48,52 +46,15 @@ export default function Cards() {
 
 	return (
 		<>
-			<div className="grid grid-cols-2 gap-3 md:grid-cols-2 sm:grid-cols-1">
+			<div className="grid grid-cols-2 gap-3 sm:grid-cols-1">
 				{list.map((item, index) => (
-					<div
-						className="flex flex-col gap-3 sm:gap-6 sm:flex-row "
+					<CardItem
+						{...item}
+						description="lorrem bla bla bla lorem"
+						image={item.img}
 						key={index}
-					>
-						<Image
-							className="card__image"
-							src={list[0].img}
-							alt={list[0].title}
-						></Image>
-						<div className="sm:flex sm:flex-col sm:justify-center">
-							<p>
-								<strong>Product 1</strong>
-							</p>
-							<p className="hidden sm:block">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Corporis consequuntur delectus doloribus nesciunt quos voluptas
-								officia optio. Adipisci totam at fugiat, enim reiciendis rerum
-								neque unde porro blanditiis dolore dignissimos.
-							</p>
-
-							<p className="text-gray-900 text-center mt-2">90$</p>
-						</div>
-					</div>
+					/>
 				))}
-				<div className="flex flex-col gap-6 sm:flex-row ">
-					<Image
-						className="card__image"
-						src={list[0].img}
-						alt={list[0].title}
-					></Image>
-					<div className="pt-2 sm:flex sm:flex-col sm:justify-center">
-						<p>
-							<strong>Product 1</strong>
-						</p>
-						<p className="hidden sm:block">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-							consequuntur delectus doloribus nesciunt quos voluptas officia
-							optio. Adipisci totam at fugiat, enim reiciendis rerum neque unde
-							porro blanditiis dolore dignissimos.
-						</p>
-
-						<p className="text-gray-900 text-center mt-2">90$</p>
-					</div>
-				</div>
 			</div>
 
 			<Divider />
