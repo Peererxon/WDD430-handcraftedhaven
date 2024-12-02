d5e86dd99e2f395f15cd2dc04bab2fc413719c2e
IsaiasZc <coroxerox2@gmail.com>
2024-12-01 19:45:35 -0500

    [FIXED] Footer fixed. now with tailwind

--- src/app/ui/Footer.tsx
+++ src/app/ui/Footer.tsx
@@ -1,39 +1,34 @@
 import React from 'react';
-import styles from '@/app/ui/footer.module.css'; 
+import FooterElement from './components/footer-element';
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footerContainer}>
-        <div className={styles.footerSection}>
-          <h3>About Us</h3>
+    <footer className="bg-neutral-800 py-5">
+      <div className="flex flex-wrap gap-x-5 px-5">
+        <FooterElement title="About Us">
           <p>Connecting you with skilled artisans for all your handyman needs.</p>
-        </div>
-        <div className={styles.footerSection}>
-          <h3>Quick Links</h3>
+        </FooterElement>
+        <FooterElement title="Quick Links">
           <ul>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#faq">FAQ</a></li>
+            <li><a className='text-[#ffd700] hover:underline hover:underline-offset-4' href="#services">Services</a></li>
+            <li><a className='text-[#ffd700] hover:underline hover:underline-offset-4' href="#about">About Us</a></li>
+            <li><a className='text-[#ffd700] hover:underline hover:underline-offset-4' href="#contact">Contact</a></li>
+            <li><a className='text-[#ffd700] hover:underline hover:underline-offset-4' href="#faq">FAQ</a></li>
           </ul>
-        </div>
-        <div className={styles.footerSection}>
+        </FooterElement>
+        <FooterElement title='Contact Us'>
           <h3>Contact Us</h3>
           <p>Email: support@handymanapp.com</p>
           <p>Phone: +1-234-567-890</p>
-        </div>
-        <div className={styles.footerSection}>
-          <h3>Follow Us</h3>
-          <div className={styles.socialIcons}>
+        </FooterElement>
+        <FooterElement title='Follow Us'>
             <a href="#" aria-label="Facebook"><i className="fab fa-facebook-f"></i></a>
             <a href="#" aria-label="Twitter"><i className="fab fa-twitter"></i></a>
             <a href="#" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
             <a href="#" aria-label="LinkedIn"><i className="fab fa-linkedin-in"></i></a>
-          </div>
-        </div>
+        </FooterElement>
       </div>
-      <div className={styles.footerBottom}>
+      <div className='text-center mt-5 text-neutral-400'>
         <p>&copy; 2024 Handyman App. All rights reserved.</p>
       </div>
     </footer>
