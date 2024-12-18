6f640aa298fc3a712ab5454592dae4a06e47e196
andersongil <andersongil.dev@gmail.com>
2024-12-18 16:20:54 -0400

    fix any on production deployment

--- src/app/ui/CardsClient.tsx
+++ src/app/ui/CardsClient.tsx
@@ -4,6 +4,7 @@ import { Divider } from "@nextui-org/divider";
 import CardItem from "./Card";
 import { Link } from "@nextui-org/react";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function CardsClient({ artWorks }: { artWorks: any[] }) {
 	return (
 		<>
